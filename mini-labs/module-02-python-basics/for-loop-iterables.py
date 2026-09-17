4249c57b6be7320d7e39e7e0a71abfefeb8f8829
p schuhmacher <contact@pschuhmacher.com>
2026-09-17 10:31:22 +0200

    upload commented solutions for module 2

---
+++ mini-labs/module-02-python-basics/for-loop-iterables.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4]
+
+# A list is an iterable. Use a for loop to go through each element
+for x in numbers:
+	print(x)
+
+string = "hello, world"
+
+# Strings are iterable too!
+for c in string:
+	print(c)
